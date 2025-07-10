38395039016e2bec7c7cb0f855a6c71c11f149b6
Aryan <bhardwaj7a@gmail.com>
2025-07-10 18:39:14 +0530

    Write the UI for the Navbar

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,7 +1,13 @@
+import DisplayHome from "./DisplayHome";
+import {Routes} from "react-router-dom";
+import { Route } from "react-router-dom";
+
  const Display = () => {
     return (
-        <div>
-
+        <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
+              <Routes>
+                <Route path="/" element = {<DisplayHome />} />
+              </Routes>
         </div>
     )
 }
